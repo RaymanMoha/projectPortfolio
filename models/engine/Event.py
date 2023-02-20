bb7d30d847d6a27d08e3f3bcf9dd9163171bae14
aTfure <atfure@gmail.com>
2023-02-20 11:23:40 +0300

    ORM database

---
+++ models/engine/Event.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Define the class Image.
+"""
+from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, String
+
+
+class Image(BaseModel):
+    """
+        Define the class Event that inherits from BaseModel.
+    """
+    __tablename__ = "event"
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    event_id = Column(String(60), ForeignKey('event.id'), nullable=False)
---
+++ models/engine/NGO.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Define the class Image.
+"""
+from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, String
+
+
+class ngo(BaseModel):
+    """
+        Define the class Image that inherits from BaseModel.
+    """
+    __tablename__ = "ngo"
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    ngo_name = Column(String(128), nullable=False)
+    ngo_path = Column(String(128), nullable=False)
+    volunteer = relationship("Volunteer", backref="ngo", cascade="delete")
+    event = relationship("Event", backref= "volunteer, ngo", cascade="delete")
