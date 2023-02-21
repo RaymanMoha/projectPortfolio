7f243ae04344ad62d483ef92269c56bd82d85916
aTfure <atfure@gmail.com>
2023-02-22 00:58:25 +0300

    ORM updated

--- models/__init__.py
+++ models/__init__.py
@@ -4,14 +4,11 @@ instance for this application"""
 
 from models.engine.file_storage import FileStorage
 from models.user import User
-from models.NGO import Ngo
-from models.volunteer import Volunteer
 from models.Event import Event
 from models.base_model import BaseModel
 from models.engine.db_storage import DBStorage
 
-classes = {"User": User, "BaseModel": BaseModel,
-           "Volunteer": Volunteer, "NGO": Ngo, "Event": Event}
+classes = {"User": User, "BaseModel": BaseModel, "Event": Event}
 
 storage = FileStorage()
 storage.reload()
