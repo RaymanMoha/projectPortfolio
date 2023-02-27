1dc8b333cbe4455b113fb1e27fcdee8c0652c4c5
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2023-02-27 20:11:45 +0300

    connect to database

--- app.py
+++ app.py
@@ -23,3 +23,23 @@ def indexNgo():
 def indexVol():
     return render_template('indexVol.html')
 
+# This code implements a Flask application with multiple routes for user login and logout.
+# Route for volunteer login. It will receive a POST request from the login form, 
+# extract the username and password, and validate the credentials against the 
+# 'Students' table in a SQl database. If the credentials are valid, it will 
+# store the user information in session variables and redirect the user to the 
+# index page. If the credentials are invalid, it will display an error message.
+
+@app.route('/loginVolunteer', methods=['GET', 'POST'])
+def loginVolunteer():
+    # Message to be displayed on the login page
+    msg = ''
+    # Check if the request is a POST request and the form has the required fields
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Extract the form data
+        username = request.form['username']
+        password = request.form['password']
+        # Connect to the database
+   
+
+
